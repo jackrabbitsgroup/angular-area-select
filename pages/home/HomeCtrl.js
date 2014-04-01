75fbe29c3648035a39cc224fab23c5ed55e2eaa4
Luke Madera <luke.madera@gmail.com>
2014-04-01 12:13:06 -0700

    feat: move instId to $scope.opts to allow setting/changing it later

--- pages/home/HomeCtrl.js
+++ pages/home/HomeCtrl.js
@@ -5,4 +5,17 @@
 
 angular.module('myApp').controller('HomeCtrl', ['$scope', function($scope) {
 	$scope.coords ={};
+	
+	$scope.coords2 ={};
+	$scope.opts2 ={
+		instId: 'my-inst'
+	};
+	
+	$scope.reInit =function() {
+		$scope.$broadcast('jrgAreaSelectReInit', {instId:$scope.opts2.instId});
+	};
+	
+	$scope.changeInstId	=function() {
+		$scope.opts2.instId ='my-new-inst'+Math.random().toString(36).substring(7);
+	};
 }]);
